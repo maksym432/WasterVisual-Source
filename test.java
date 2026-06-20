a23bb4b40f353a0449d94c3ab377b86fd44bb14b
maksym432 <maksym432@users.noreply.github.com>
2026-06-20 13:19:14 +0200

    chore: remove test.java and old log files

--- test.java
+++
@@ -1,2 +0,0 @@
-import net.minecraft.client.network.OtherClientPlayerEntity;
-class Test { void a(net.minecraft.client.MinecraftClient c) { new OtherClientPlayerEntity(c.world, c.player.getGameProfile()); } }
